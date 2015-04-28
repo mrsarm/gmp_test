bbf4a30110afda8ef45724e0e74e7e00c2aae533
Mariano Ruiz <mrsarm@gmail.com>
2015-04-28 02:16:29 -0300

    Added headers to source files. Removed unused imports

--- src/date.c
+++ src/date.c
@@ -1,12 +1,26 @@
-//
-// Created by user on 28/04/15.
-//
+/* date.c
+
+   Copyright (C) 2015 Mariano Ruiz <mrsarm@gmail.com>
+   This file is part of the gmp_test C-project.
+
+   The gmp_test C-project is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+
 #define _POSIX_C_SOURCE 200809L
 
 #include "date.h"
-
-#include <inttypes.h>
-#include <math.h>
 #include <stdio.h>
 #include <time.h>
 
--- src/main.c
+++ src/main.c
@@ -17,6 +17,7 @@
    License along with the GNU C Library; if not, see
    <http://www.gnu.org/licenses/>.  */
 
+
 #include <gmp.h>
 #include <stdio.h>
 #include "date.h"
